fb0f7c5e56c39c7efcee3b9b835f6694afdd3d29
EC2 Default User <ec2-user@ip-172-31-22-83.ap-northeast-1.compute.internal>
2022-10-13 14:26:52 +0000

    [Finish]4-16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) #urlに記載されているidのレコードをUserモデルに取得し、変数に格納する
-    @post_images = @user.post_images　#特定のユーザ(@user)に関連付けられた投稿全て(.post_image)を取得し、変数に渡す
+    @post_images = @user.post_images #特定のユーザ(@user)に関連付けられた投稿全て(.post_images)を取得し、変数に渡す
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
